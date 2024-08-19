0c87669178322115cc0f8f99d540823c30552c2e
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-08-19 15:43:23 -0700

    update settings

--- asaiah_portfolio/settings.py
+++ asaiah_portfolio/settings.py
@@ -101,9 +101,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
